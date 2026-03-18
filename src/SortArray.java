2945ba98e1f0a5f357ebffdc862ec3c8a9734eb1
Mohanaprabu2004 <Mohanaprabu2004@gmail.com>
2026-03-18 14:54:35 +0530

    Initial Commit

---
+++ src/SortArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] a={63,5,3,5,2,5,6,7,3,5,6};
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
